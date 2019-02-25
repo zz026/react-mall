aea91bf2fb6085a1420b3c96371ad30cc56523dc
zhengwei <zzw1366@163.com>
2019-02-25 23:16:41 +0800

    登录界面

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,7 +1,129 @@
 import React, { Component } from 'react';
+import { Form, Input, Button, Checkbox } from 'antd';
+import './login.less'
 
-export default class Login extends Component {
+class Login extends Component {
+
+  state = {
+    confirmDirty: false
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.form.validateFieldsAndScroll((err, values) => {
+      if (!err) {
+        console.log('Received values of form: ', values);
+      }
+    });
+  }
+
+  // 密码验证
+  validateToNextPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && this.state.confirmDirty) {
+      form.validateFields(['confirm'], { force: true });
+    }
+    callback();
+  }
+
+  // 二次确认密码
+  handleConfirmBlur = (e) => {
+    const value = e.target.value;
+    this.setState({ confirmDirty: this.state.confirmDirty || !!value });
+  }
+
+  compareToFirstPassword = (rule, value, callback) => {
+    const form = this.props.form;
+    if (value && value !== form.getFieldValue('password')) {
+      callback('两次密码不一致!');
+    } else {
+      callback();
+    }
+  }
+
   render() {
-    return <div>登录</div>
+    const { getFieldDecorator } = this.props.form;
+    const formItemLayout = {
+      labelCol: {
+        xs: { span: 24 },
+        sm: { span: 4 },
+      },
+      wrapperCol: {
+        xs: { span: 24 },
+        sm: { span: 20 },
+      },
+    };
+    const tailFormItemLayout = {
+      wrapperCol: {
+        xs: {
+          span: 24,
+          offset: 0,
+        },
+        sm: {
+          span: 16,
+          offset: 8,
+        },
+      },
+    };
+    return (
+      <div className="loginForm">
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Item
+            {...formItemLayout}
+            label="用户名"
+          >
+            {getFieldDecorator('userName', {
+              rules: [
+                { required: true, message: '请输入用户名'}
+              ],
+            })(
+              <Input />
+            )}
+          </Form.Item>
+          <Form.Item
+            {...formItemLayout}
+            label="密码"
+          >
+            {getFieldDecorator('password', {
+              rules: [{
+                required: true, message: '请输入密码!',
+              }, {
+                validator: this.validateToNextPassword,
+              }],
+            })(
+              <Input type="password" />
+            )}
+          </Form.Item>
+          <Form.Item
+            {...formItemLayout}
+            label="再次输入密码"
+          >
+            {getFieldDecorator('confirm', {
+              rules: [{
+                required: true, message: '请再次输入密码!',
+              }, {
+                validator: this.compareToFirstPassword,
+              }],
+            })(
+              <Input type="password" onBlur={this.handleConfirmBlur} />
+            )}
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            {getFieldDecorator('agreement', {
+              valuePropName: 'checked',
+            })(
+              <Checkbox>我已阅读 <a href="">《霸王条约》</a></Checkbox>
+            )}
+          </Form.Item>
+          <Form.Item {...tailFormItemLayout}>
+            <Button htmlType="submit">注册</Button>
+          </Form.Item>
+      </Form>
+    </div>
+    )
   }
 }
+
+const LoginForm = Form.create()(Login);
+
+export default LoginForm;
--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import { HashRouter, Route, Switch, Redirect } from 'react-router-dom';
 import App from '../App';
 import Login from '../pages/login'
 import Regist from '../pages/regist'
+import Home from '../pages/home'
 
 export default class Router extends Component {
+  state = {
+    hasLogin: false
+  }
+  getDefaultRoute() {
+    return this.state.hasLogin ? '/home' : '/login';
+  }
   render() {
     return (
       <HashRouter>
         <App>
           <Switch>
+            <Route path='/' exact render={()=> (
+               <Redirect to='home' />
+            )}/>
             <Route path='/login' component={Login}></Route>
             <Route path='/regist' component={Regist}></Route>
+            <Route path='/home' component={Home}></Route> 
           </Switch>
         </App>
       </HashRouter>
